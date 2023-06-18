236df4020abd8fdbfa6c982525ad75cec1905e4f
Sergey Vasiliev <sdv22@live.ru>
2023-06-18 10:44:10 +0300

    Allow IP address for private DNS
    Change-Id: I79dcb190023400fbc3eca661074a8775e0345ab1

--- src/com/android/settings/network/PrivateDnsModeDialogPreference.java
+++ src/com/android/settings/network/PrivateDnsModeDialogPreference.java
@@ -56,6 +56,7 @@ import com.android.settingslib.RestrictedLockUtils;
 import com.android.settingslib.RestrictedLockUtilsInternal;
 
 import com.google.common.net.InternetDomainName;
+import com.google.common.net.InetAddresses;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -344,7 +345,7 @@ public class PrivateDnsModeDialogPreference extends CustomDialogPreferenceCompat
         final Button saveButton = getSaveButton();
         if (saveButton != null) {
             saveButton.setEnabled(modeProvider
-                    ? InternetDomainName.isValid(mEditText.getText().toString())
+                    ? (InternetDomainName.isValid(mEditText.getText().toString()) || InetAddresses.isInetAddress(mEditText.getText().toString()) )
                     : true);
         }
     }
